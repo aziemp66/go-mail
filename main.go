2eab214ef2147d52cb061a826e7ca242c91440d0
Azie Melza Pratama <aziemp66@gmail.com>
2022-12-14 14:31:48 +0700

    Revising Code

--- main.go
+++ main.go
@@ -7,13 +7,12 @@ import (
 	"github.com/aziemp66/go-mail/helper"
 )
 
-func sendMailSimple(mail string, appPassword string, to string, subject string, body string) {
+func sendMailSimple(mail string, appPassword string, to []string, subject string, body string) {
 	auth := smtp.PlainAuth("", mail, appPassword, "smtp.gmail.com")
-	toList := []string{to}
 
-	msg := []byte("To: " + to + "\r" + "Subject: " + subject + "\r" + "\r" + body + "\r")
+	msg := []byte("Subject: " + subject + "\r" + "\r" + body + "\r")
 
-	err := smtp.SendMail("smtp.gmail.com:587", auth, mail, toList, msg)
+	err := smtp.SendMail("smtp.gmail.com:587", auth, mail, to, msg)
 
 	if err != nil {
 		fmt.Println("Error: ", err)
@@ -23,5 +22,5 @@ func sendMailSimple(mail string, appPassword string, to string, subject string, 
 func main() {
 	cfg := helper.LoadConfig()
 
-	sendMailSimple(cfg.APP_EMAIL, cfg.APP_PASSWORD, "azielala55@gmail.com", "Test", "Welcome to Go")
+	sendMailSimple(cfg.APP_EMAIL, cfg.APP_PASSWORD, []string{"azielala55@gmail.com"}, "Hello", "Welcome to SMTP With Go")
 }
