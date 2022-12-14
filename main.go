f46fa5237815ade9986c5e3ce56c570ad59d1bea
Azie Melza Pratama <aziemp66@gmail.com>
2022-12-14 15:06:43 +0700

    Adding Simple Html Send Mail Method

--- main.go
+++ main.go
@@ -19,8 +19,35 @@ func sendMailSimple(mail string, appPassword string, to []string, subject string
 	}
 }
 
+func sendMailSimpleHtml(mail string, appPassword string, to []string, subject string, html string) {
+	auth := smtp.PlainAuth("", mail, appPassword, "smtp.gmail.com")
+
+	header := make(map[string]string)
+	header["From"] = mail
+	header["To"] = to[0]
+	header["Subject"] = subject
+	header["MIME-Version"] = "1.0"
+	header["Content-Type"] = "text/html; charset=\"UTF-8\""
+	header["Content-Transfer-Encoding"] = "base64"
+
+	message := ""
+	for k, v := range header {
+		message += fmt.Sprintf("%s: %s\r", k, v)
+	}
+
+	message += "\r" + html
+
+	err := smtp.SendMail("smtp.gmail.com:587", auth, mail, to, []byte(message))
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+}
+
 func main() {
 	cfg := helper.LoadConfig()
 
-	sendMailSimple(cfg.APP_EMAIL, cfg.APP_PASSWORD, []string{"azielala55@gmail.com"}, "Hello", "Welcome to SMTP With Go")
+	// sendMailSimple(cfg.APP_EMAIL, cfg.APP_PASSWORD, []string{"azielala55@gmail.com"}, "Hello", "Welcome to SMTP With Go")
+	sendMailSimpleHtml(cfg.APP_EMAIL, cfg.APP_PASSWORD, []string{"aziemp55@gmail.com"}, "Hello", "<h1>Welcome to SMTP With Go</h1><p>My Name Is Gustavo Fring, You Can Call me Gus</p>")
 }
